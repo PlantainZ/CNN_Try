670a5612180a901c2aa22449decf8ee2e5d662dc
柳沉翐 <593525228@qq.com>
2020-12-25 00:54:47 +0800

    20201225 patch

--- 02_KaggleTry/20201224_housePMine.py
+++ 02_KaggleTry/20201224_housePMine.py
@@ -11,7 +11,9 @@ import pandas as pd
 from mxnet import nd
 
 # 1 # 读取数据 / 练习==============================================
-csv_path = "D:\plantainz\OuO_DeepLearning\DataSet\housePrices"
+# csv_path = "D:\plantainz\OuO_DeepLearning\DataSet\housePrices" # company
+csv_path = "C:\XXXX_Learning\XX_DataSet\kaggle_house_price" # home
+
 train_data = pd.read_csv(csv_path + '\\train.csv')
 test_data = pd.read_csv(csv_path + '\\test.csv')
 all_features = pd.concat((train_data.iloc[:,1:-1],test_data.iloc[:,1:]))
@@ -19,13 +21,17 @@ all_features = pd.concat((train_data.iloc[:,1:-1],test_data.iloc[:,1:]))
 # 2 # 预处理
 # > 连续 + 离散处理 > 重组
 numeric_features = all_features.dtypes[all_features.dtypes!='object'].index
-print(type(all_features.dtypes))
+print(all_features.dtypes)
+
 all_features[numeric_features] = all_features[numeric_features].apply(
     lambda x: (x-x.mean()) / x.std()
 )
+
 all_features[numeric_features] = all_features[numeric_features].fillna(0)
 all_features = pd.get_dummies(all_features,dummy_na=True)
 print(all_features.shape)
+
+
 
 n_train = train_data.shape[0]
 train_features = nd.array(all_features[:n_train].values)
@@ -34,6 +40,7 @@ train_labels = nd.array(train_data.SalePrice.values)
 train_labels = train_labels.reshape((-1,1))
 
 # ooooo走你！
+
 
 
 
